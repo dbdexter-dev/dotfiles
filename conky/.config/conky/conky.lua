a2b906055a86a0caaecc07bf15c44b3ac514c5f3
dbdexter-dev <dbdexter.dev@gmail.com>
2017-05-05 22:59:47 +0200

    Added more meters and better positioning

--- conky/.config/conky/conky.lua
+++ conky/.config/conky/conky.lua
@@ -2,15 +2,19 @@ require 'cairo'
 
 font = 'SFNS Display'
 font_size = 24
+smallfont_size = 12
 interline = 5
-clock_offset_x = 0
 red,green,blue = 1,1,1
 inactiveAlpha,activeAlpha = 0.2,1
 line_width = 5
 font_slant = CAIRO_FONT_SLANT_NORMAL
 font_face = CAIRO_FONT_WEIGHT_NORMAL
-ring_x = 275
+ring_x,ring_y = 275, 0
 ring_radius = 50
+clock_x,clock_y = 0, 55
+
+up_max = 300
+down_max = 3000
 
 function conky_main()
 	if conky_window == nil then
@@ -31,17 +35,21 @@ function conky_main()
 		cairo_set_line_width(cr, line_width)
 		cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND)
 
-		load_gauge(cr, ring_x, ring_radius+line_width, ring_radius)
-		temp_gauge(cr, ring_x, ring_radius+line_width+2*ring_radius, ring_radius-line_width)
-		mem_gauge(cr, ring_x, ring_radius+2*line_width+4*ring_radius, ring_radius-line_width)
+		load_gauge(cr, ring_x, ring_y+ring_radius+line_width, ring_radius)
+		temp_gauge(cr, ring_x, ring_y+ring_radius+line_width+2*ring_radius, ring_radius-line_width)
+		mem_gauge(cr, ring_x, ring_y+ring_radius+2*line_width+4*ring_radius, ring_radius-line_width)
+		net_gauge(cr, ring_x, ring_y+ring_radius+4*line_width+6*ring_radius, ring_radius)
 
+		cairo_set_font_size(cr, smallfont_size)
+	end
+
+	if updates > 3 then
 		cairo_set_font_size(cr, font_size)
-		cairo_move_to(cr, 0, 0)
 
-		clock(cr)
-
+		clock(cr, clock_x, clock_y)
 		cairo_stroke(cr)
 	end
+
 
 	cairo_destroy(cr)
 	cairo_surface_destroy(cs)
@@ -57,36 +65,36 @@ function load_gauge(cr, x, y, radius)
 	local minute15_angle = tonumber(conky_parse('${loadavg 3}'))*scale
 	local line_width = line_width*1.1
 
-	cairo_set_font_size(cr, 12)
+	cairo_set_font_size(cr, smallfont_size)
 	cairo_move_to(cr, x-16, y)
 	cairo_show_text(cr, conky_parse('${loadavg 1}'))
 
-	cairo_set_source_rgba(cr, 0.39, 0.48, 0.24, activeAlpha)
+	cairo_set_source_rgba(cr, 0.55, 0.76, 0.29, activeAlpha)
 	cairo_move_to(cr, x, y-radius)
 	cairo_arc(cr, x, y, radius, -math.pi/2, (minute1_angle - math.pi/2))
 	cairo_stroke(cr)
 
-	cairo_set_source_rgba(cr, 0.3, 0.4, 0.21, activeAlpha)
+	cairo_set_source_rgba(cr, 0.48, 0.70, 0.26, activeAlpha)
 	cairo_move_to(cr, x, y-radius+line_width)
 	cairo_arc(cr, x, y, radius-line_width, -math.pi/2, (minute5_angle - math.pi/2))
 	cairo_stroke(cr)
 
-	cairo_set_source_rgba(cr, 0.26, 0.37, 0.17, activeAlpha)
+	cairo_set_source_rgba(cr, 0.33, 0.55, 0.18, activeAlpha)
 	cairo_move_to(cr, x, y-radius+2*line_width)
 	cairo_arc(cr, x, y, radius-2*line_width, -math.pi/2, (minute15_angle - math.pi/2))
 	cairo_stroke(cr)
 end
 
 function temp_gauge(cr, x, y, radius)
-	local temp = conky_parse('${hwmon temp 2}')
-	cairo_set_font_size(cr, 12)
+	local temp = conky_parse('${hwmon 1 temp 1}')
+	cairo_set_font_size(cr, smallfont_size)
 	cairo_set_source_rgba(cr, red, green, blue, activeAlpha)
-	cairo_move_to(cr, x-12, y)
+	cairo_move_to(cr, x-18, y)
 	cairo_show_text(cr, temp.."°C")
 
 	local temp_angle = temp/100*math.pi
 
-	cairo_set_source_rgba(cr, 0.8, 0.08, 0.22, activeAlpha)
+	cairo_set_source_rgba(cr, 0.90, 0.22, 0.21, activeAlpha)
 	cairo_move_to(cr, x, y-radius)
 	cairo_arc(cr, x, y, radius, -math.pi/2, (temp_angle - math.pi/2))
 	cairo_stroke(cr)
@@ -94,21 +102,54 @@ end
 
 function mem_gauge(cr, x, y, radius)
 	local memperc = tonumber(conky_parse('${memperc}'))
-	cairo_set_font_size(cr, 12)
+	cairo_set_font_size(cr, smallfont_size)
 	cairo_set_source_rgba(cr, red, green, blue, activeAlpha)
-	cairo_move_to(cr, x-20, y)
+	cairo_move_to(cr, x-24, y)
 	cairo_show_text(cr, conky_parse('${mem}'))
 
 	local mem_angle = memperc/100*math.pi
 
-	cairo_set_source_rgba(cr, 0.07, 0.62, 0.8, activeAlpha)
+	cairo_set_source_rgba(cr, 0.99, 0.84, 0.21, activeAlpha)
 	cairo_move_to(cr, x, y-radius)
 	cairo_arc(cr, x, y, radius, -math.pi/2, (mem_angle - math.pi/2))
 	cairo_stroke(cr)
 
 end
 
-function clock(cr)
+function net_gauge(cr, x, y, radius)
+	local function clamp(val, max)
+		if val > max then
+			return max
+		end
+		return val
+	end
+
+	local upkb = tonumber(conky_parse('${upspeedf wlp58s0}'))
+	local downkb = tonumber(conky_parse('${downspeedf wlp58s0}'))
+
+	local up_angle = (1-clamp(upkb, up_max)/up_max)*math.pi
+	local down_angle = clamp(downkb, down_max)/down_max*math.pi
+
+	cairo_set_font_size(cr, smallfont_size)
+	cairo_set_source_rgba(cr, red, green, blue, activeAlpha)
+	cairo_move_to(cr, x-24, y-smallfont_size/2)
+	cairo_show_text(cr, conky_parse('${downspeed wlp58s0}'))
+	cairo_move_to(cr, x-24, y+smallfont_size/2)
+	cairo_show_text(cr, conky_parse('${upspeed wlp58s0}'))
+
+	cairo_set_source_rgba(cr, 0.16, 0.63, 0.87, activeAlpha)
+	cairo_move_to(cr, x, y-radius)
+	cairo_arc(cr, x, y, radius, -math.pi/2, (down_angle - math.pi/2))
+	cairo_stroke(cr)
+
+	cairo_move_to(cr, x, y+radius-line_width)
+	cairo_set_source_rgba(cr, 0.96, 0.49, 0, activeAlpha)
+	cairo_arc_negative(cr, x, y, radius-line_width, math.pi/2, (up_angle-math.pi/2))
+	cairo_stroke(cr)
+
+end
+
+function clock(cr, x, y)
 	local hour = tonumber(conky_parse('${exec date +"%I"}'))
 	local minute = tonumber(conky_parse('${exec date +"%M"}'))
 
@@ -168,45 +209,45 @@ function clock(cr)
 	end
 
 
-	cairo_move_to(cr, clock_offset_x, (font_size+interline))
+	cairo_move_to(cr, x, y+(font_size+interline))
 	clock_show_text(cr, "IT", true)
 	clock_show_text(cr, "L", false)
 	clock_show_text(cr, "IS", true)
 	clock_show_text(cr, "ASTIME", false)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*2)
+	cairo_move_to(cr, x, y+(font_size+interline)*2)
 	clock_show_text(cr, "A", quarter)
 	clock_show_text(cr, "C", false)
 	clock_show_text(cr, "QUARTER", quarter)
 	clock_show_text(cr, "DI", false)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*3)
+	cairo_move_to(cr, x, y+(font_size+interline)*3)
 	clock_show_text(cr, "TWENTY", twenty)
 	clock_show_text(cr, "FIVE", five)
 	clock_show_text(cr, "X", false)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*4)
+	cairo_move_to(cr, x, y+(font_size+interline)*4)
 	clock_show_text(cr, "HALF", half)
 	clock_show_text(cr, "B", false)
 	clock_show_text(cr, "TEN", ten)
 	clock_show_text(cr, "F", false)
 	clock_show_text(cr, "TO", to)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*5)
+	cairo_move_to(cr, x, y+(font_size+interline)*5)
 	clock_show_text(cr, "PAST", past)
 	clock_show_text(cr, "ERU", false)
 	clock_show_text(cr, "NINE", hrnine)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*6)
+	cairo_move_to(cr, x, y+(font_size+interline)*6)
 	clock_show_text(cr, "ONE", hrone)
 	clock_show_text(cr, "SIX", hrsix)
 	clock_show_text(cr, "THREE", hrthree)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*7)
+	cairo_move_to(cr, x, y+(font_size+interline)*7)
 	clock_show_text(cr, "FOUR", hrfour)
 	clock_show_text(cr, "FIVE", hrfive)
 	clock_show_text(cr, "TWO", hrtwo)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*8)
+	cairo_move_to(cr, x, y+(font_size+interline)*8)
 	clock_show_text(cr, "EIGHT", hreight)
 	clock_show_text(cr, "ELEVEN", hreleven)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*9)
+	cairo_move_to(cr, x, y+(font_size+interline)*9)
 	clock_show_text(cr, "SEVEN", hrseven)
 	clock_show_text(cr, "TWELVE", hrtwelve)
-	cairo_move_to(cr, clock_offset_x, (font_size+interline)*10)
+	cairo_move_to(cr, x, y+(font_size+interline)*10)
 	clock_show_text(cr, "TEN", hrten)
 	clock_show_text(cr, "SE", false)
 	clock_show_text(cr, "OCLOCK", oclock)
